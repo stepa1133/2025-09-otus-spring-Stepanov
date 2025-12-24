564dfbac016e88f9d64af8e47a6a5b034504cebf
Alexander Stepanov <stepa113@mail.ru>
2025-12-24 17:47:15 +0300

    finish commit

--- hw-09-spring-data-mvc/src/test/java/ru/otus/hw/AuthorControllerTest.java
+++ hw-09-spring-data-mvc/src/test/java/ru/otus/hw/AuthorControllerTest.java
@@ -1,16 +1,12 @@
 package ru.otus.hw;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.otus.hw.controller.AuthorController;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import ru.otus.hw.dto.AuthorDto;
-import ru.otus.hw.models.Author;
-import ru.otus.hw.services.AuthorService;
 import ru.otus.hw.services.AuthorServiceImpl;
 
 import java.util.List;
