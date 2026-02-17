f8d07377286295d169107729470bfa530392ac15
Alexander Stepanov <stepa113@mail.ru>
2026-02-17 07:07:44 +0300

    initial commit

---
+++ hw13-spring-security-authorization/src/main/java/ru/otus/hw/security/SecurityConfiguration.java
@@ -0,0 +1,51 @@
+package ru.otus.hw.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Arrays;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers
+                        .frameOptions(frame -> frame.disable())
+                )
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .authorizeHttpRequests((authorize) -> authorize
+                               .requestMatchers("/h2-console/**").permitAll()
+                               .anyRequest().authenticated()
+                )
+
+                .formLogin(fm -> fm
+                        .defaultSuccessUrl("/"))
+                .rememberMe(rm -> rm.key("AnyKey")
+                        .tokenValiditySeconds(2000));
+        return http.build();
+    }
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    public static void main(String[] args) {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        String [] arrPasswords = new String[] {"admin", "tests"};
+        Arrays.stream(arrPasswords).map(passwordEncoder::encode).forEach(System.out::println);
+    }
+
+}
