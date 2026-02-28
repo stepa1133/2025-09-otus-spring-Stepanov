955e436d24b5f6bfea5e99b5230488e13cb55253
Alexander Stepanov <stepa113@mail.ru>
2026-02-28 18:00:47 +0300

    initial commit(copy hw spring data)

---
+++ hw14-spring-batch/src/test/java/ru/otus/hw/integration/BookServiceIntegrationTest.java
@@ -0,0 +1,160 @@
+package ru.otus.hw.integration;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.hw.converters.BookConverter;
+import ru.otus.hw.converters.dto.BookDtoConverter;
+import ru.otus.hw.dto.BookDto;
+import ru.otus.hw.models.Book;
+import ru.otus.hw.services.BookServiceImpl;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+
+@DisplayName("Тесты сервиса книг")
+@DataJpaTest
+@ComponentScan("ru.otus.hw")
+@Transactional(propagation = Propagation.NEVER)
+public class BookServiceIntegrationTest {
+
+    @Autowired
+    private BookServiceImpl bookService;
+
+    @Autowired
+    private BookConverter bookConverter;
+
+    @Autowired
+    private BookDtoConverter bookDtoConverter;
+
+    @DisplayName("Не должен выбрасывать исключния при загрузке всех книг")
+    @Test
+    void shouldDoNotThrowException1() {
+        assertThatCode(
+                () -> bookService.findAll().stream()
+                        .map(bookDtoConverter::toDomain)
+                        .map(bookConverter::bookToString)
+                        .collect(Collectors.joining("," + System.lineSeparator())))
+        .doesNotThrowAnyException();
+    }
+
+    @DisplayName("Не должен выбрасывать исключния при загрузке книги по итендификатору")
+    @Test
+    void shouldDoNotThrowException2() {
+        assertThatCode(
+                () -> bookService.findById(1)
+                        .map(bookDtoConverter::toDomain)
+                        .map(bookConverter::bookToString))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("Не должен выбрасывать исключния при вставке новой книги")
+    @Test
+    void shouldDoNotThrowException3() {
+        assertThatCode(
+                () -> {
+                    var savedBook = bookService.insert("title", 1, 1);
+                    Book book = bookDtoConverter.toDomain(savedBook);
+                    bookConverter.bookToString(book);
+                }
+        ).doesNotThrowAnyException();
+    }
+
+    @DisplayName("Не должен выбрасывать исключния при обновлении книги")
+    @Test
+    void shouldDoNotThrowException4() {
+        assertThatCode(
+                () -> {
+                    var savedBook = bookService.update(1, "title", 1, 1);
+                    Book book = bookDtoConverter.toDomain(savedBook);
+                    bookConverter.bookToString(book);
+                }
+        ).doesNotThrowAnyException();
+    }
+
+
+
+    @DisplayName("Должен найти книгу по id")
+    @Test
+    void shouldFindBookById() {
+        Optional<BookDto> book = bookService.findById(1);
+
+        assertThat(book)
+                .isPresent()
+                .get()
+                .extracting(BookDto::getId)
+                .isEqualTo(1L);
+    }
+
+    @DisplayName("Должен выгружать список всех книг")
+    @Test
+    void shouldReturnCorrectBooksList() {
+        List<BookDto> books = bookService.findAll();
+
+        assertThat(books)
+                .extracting(BookDto::getId)
+                .containsExactlyInAnyOrder(1L, 2L, 3L);
+
+    }
+
+    @DisplayName("должен сохранять новую книгу")
+    @Test
+    @Transactional
+    void shouldSaveNewBook() {
+        bookService.insert("Как учить студентов и не поехать кукухой", 1, 1);
+        Optional<BookDto> book = bookService.findById(4);
+        assertThat(book)
+                .isPresent()
+                .get()
+                .extracting(BookDto::getTitle)
+                .isEqualTo("Как учить студентов и не поехать кукухой");
+    }
+
+    @DisplayName("должен обновлять книгу")
+    @Test
+    void shouldUpdateBook() {
+        Optional<BookDto> bookDto = bookService.findById(1);
+        assertThat(bookDto)
+                .isPresent()
+                .get()
+                .extracting(BookDto::getTitle)
+                .isNotEqualTo("Как учить студентов и не поехать кукухой");
+        Book book = bookDtoConverter.toDomain(bookDto.get());
+
+
+
+        bookService.update(book.getId(), "Как учить студентов и не поехать кукухой", book.getAuthor().getId(), book.getGenre().getId());
+
+        Optional<BookDto> bookDtoAfter = bookService.findById(1);
+        assertThat(bookDtoAfter)
+                .isPresent()
+                .get()
+                .extracting(BookDto::getTitle)
+                .isEqualTo("Как учить студентов и не поехать кукухой");
+    }
+
+    @DisplayName("должен удалять книгу")
+    @Test
+    @Transactional
+    void shouldDeleteBook() {
+        Optional<BookDto> existingBook = bookService.findById(1);
+        assertThat(existingBook).isPresent();
+
+        bookService.deleteById(1);
+
+        assertThatThrownBy(
+                () -> bookService.findById(1)
+        );
+
+    }
+}
