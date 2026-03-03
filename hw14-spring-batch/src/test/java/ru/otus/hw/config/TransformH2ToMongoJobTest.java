25165bacf18be1a7ab6b38f56ce0cafabaf9cb1f
Alexander Stepanov <stepa113@mail.ru>
2026-03-03 12:44:34 +0300

    add test

--- hw14-spring-batch/src/test/java/ru/otus/hw/config/TransformH2ToMongoJobTest.java
+++ hw14-spring-batch/src/test/java/ru/otus/hw/config/TransformH2ToMongoJobTest.java
@@ -11,7 +11,6 @@ import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.JobRepositoryTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -36,7 +35,7 @@ public class TransformH2ToMongoJobTest {
     }
 
     @Test
-    public void donla() throws Exception{
+    public void test() throws Exception{
         System.out.println("TEST STARTED");
 
         Job job = jobLauncherTestUtils.getJob();
